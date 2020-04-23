3dfe5c1f820e3d3647a97fe163ea4a0c4dcce155
Dennise <63337204+dentolos19@users.noreply.github.com>
2020-04-23 08:09:45 +0800

    Added Update Checker

---
+++ Assets/Scripts/Configuration.cs
@@ -0,0 +1,28 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.IO;
+using System.Xml.Serialization;
+using UnityEngine;
+
+public class UserConfiguration
+{
+
+    private static readonly string Source = Path.Combine(Application.persistentDataPath, "DodgeTheBlocks.cfg");
+    private static readonly XmlSerializer Serializer = new XmlSerializer(typeof(UserConfiguration));
+
+    public int HighestScore { get; set; } = 0;
+
+    public void Save()
+    {
+        using (var stream = new FileStream(Source, FileMode.Create))
+            Serializer.Serialize(stream, this);
+    }
+
+    public static UserConfiguration Load()
+    {
+        if (!File.Exists(Source))
+            return new UserConfiguration();
+        using (var stream = new FileStream(Source, FileMode.Open))
+            return Serializer.Deserialize(stream) as UserConfiguration;
+    }
+
+}
---
+++ Assets/Scripts/Startup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Net;
+using UnityEngine;
+
+public static class GameStartup
+{
+
+    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+    public static void Startup()
+    {
+        Screen.sleepTimeout = SleepTimeout.NeverSleep;
+    }
+
+    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.AfterSceneLoad)]
+    public static void CheckForUpdates()
+    {
+        if (Application.platform == RuntimePlatform.WebGLPlayer)
+            return;
+        if (Application.internetReachability == NetworkReachability.NotReachable)
+            return;
+        var client  = new WebClient();
+        var data = client.DownloadString("https://raw.githubusercontent.com/dentolos19/DodgeTheBlocks/master/VERSION");
+        client.Dispose();
+        if (Version.Parse(data) > Version.Parse(Application.version))
+            Utilities.ShowToastAndroid("Updates are available.");
+    }
+
+}

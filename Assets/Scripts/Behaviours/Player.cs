8bece38ffede2d195710bd94614720b5a54902f3
Dennise Catolos <contact@dennise.me>
2020-06-18 15:07:30 +0800

    Fixed Score Counter Issues

--- Assets/Scripts/Behaviours/Player.cs
+++ Assets/Scripts/Behaviours/Player.cs
@@ -8,63 +8,64 @@ public class Player : MonoBehaviour
     public static int Score { get; private set; }
     public static int Deaths { get; set; }
 
-    private int mControlType;
-    private float mSensitivity;
-    private bool mDied;
-    private Camera mCamera;
-    private Rigidbody2D mRigidbody;
+    private int _controlType;
+    private float _sensitivity;
+    private bool _died;
+    private Camera _camera;
+    private Rigidbody2D _rigidbody;
 
     public TextMeshProUGUI counter;
 
     private void Start()
     {
-        mControlType = Game.Settings.ControlType;
-        mSensitivity = Game.Settings.Sensitivity;
-        mCamera = Camera.main;
-        mRigidbody = GetComponent<Rigidbody2D>();
+        _controlType = Game.Settings.ControlType;
+        _sensitivity = Game.Settings.Sensitivity;
+        _camera = Camera.main;
+        _rigidbody = GetComponent<Rigidbody2D>();
+        Score = 0;
     }
 
     private void FixedUpdate()
     {
         var input = 0f;
-        switch (mControlType)
+        switch (_controlType)
         {
             case 0:
-                input = Input.GetAxis("Horizontal") * mSensitivity * Time.fixedDeltaTime;
+                input = Input.GetAxis("Horizontal") * _sensitivity * Time.fixedDeltaTime;
                 break;
             case 1:
-                input = Input.acceleration.x * mSensitivity * Time.fixedDeltaTime;
+                input = Input.acceleration.x * _sensitivity * Time.fixedDeltaTime;
                 break;
             case 2:
                 if (Input.touches.Length <= 0)
                     return;
                 var touch = Input.GetTouch(0).position;
-                var touchPos = mCamera.ScreenToWorldPoint(touch);
+                var touchPos = _camera.ScreenToWorldPoint(touch);
                 touchPos.x = Mathf.Clamp(touchPos.x, -6, 6);
                 touchPos.y = -4;
                 touchPos.z = 0;
                 transform.position = touchPos;
                 return;
         }
-        var pos = mRigidbody.position + Vector2.right * input;
+        var pos = _rigidbody.position + Vector2.right * input;
         pos.x = Mathf.Clamp(pos.x, -6, 6);
         pos.y = -4;
-        mRigidbody.MovePosition(pos);
+        _rigidbody.MovePosition(pos);
     }
 
     private void OnCollisionEnter2D(Collision2D other)
     {
-        if (other.collider.CompareTag("Goal") || mDied)
+        if (other.collider.CompareTag("Goal") || _died)
             return;
         Deaths++;
-        mDied = true;
+        _died = true;
         Debug.Log(Deaths);
         StartCoroutine(End());
     }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.CompareTag("Obstacle") || mDied)
+        if (other.CompareTag("Obstacle") || _died)
             return;
         Score++;
         counter.text = Score.ToString();

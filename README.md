8b6e5dd21cbcd6dc4e4fe33834a17cf869aef501
Dennise Catolos <dentolos19@gmail.com>
2021-10-04 15:39:32 +0800

    Added minor changes

--- README.md
+++ README.md
@@ -1,8 +1,6 @@
 # <img src="./.github/icon.png" width="24"/> Block Dodger ![](https://img.shields.io/badge/Status-Active-green?style=flat-square&logo=codesandbox)
 
-A simple endless block dodging game made with Unity!
-
-This project was based on [Brackeys' live stream](https://youtu.be/tyAutnOlsfA).
+A simple endless block dodging game made with Unity! This project was based on [Brackeys' live stream](https://youtu.be/tyAutnOlsfA).
 
 ## Screenshots
 
--- src/Block Dodger/Assets/Scripts/Behaviours/PlayerControls.cs
+++ src/Block Dodger/Assets/Scripts/Behaviours/PlayerControls.cs
@@ -1,9 +1,9 @@
-using System;
 using UnityEngine;
 
 public class PlayerControls : MonoBehaviour
 {
 
+    private GameMaster _gameMaster;
     private Rigidbody2D _rigidbody;
 
     public float movementSpeed = 15;
@@ -11,28 +11,29 @@ public class PlayerControls : MonoBehaviour
 
     private void Start()
     {
+        _gameMaster = FindObjectOfType<GameMaster>();
         _rigidbody = GetComponent<Rigidbody2D>();
     }
 
     private void FixedUpdate()
     {
-        var accelerationMovement = Input.acceleration.x * movementSpeed * Time.fixedDeltaTime;
+        var gyroMovement = Input.acceleration.x * movementSpeed * Time.fixedDeltaTime;
         var axisMovement = Input.GetAxis("Horizontal") * movementSpeed * Time.fixedDeltaTime;
-        var position = _rigidbody.position + Vector2.right * (axisMovement + accelerationMovement);
-        position.x = Mathf.Clamp(position.x, -movementWidth, movementWidth);
-        _rigidbody.MovePosition(position);
+        var newPosition = _rigidbody.position + Vector2.right * (axisMovement + gyroMovement);
+        newPosition.x = Mathf.Clamp(newPosition.x, -movementWidth, movementWidth);
+        _rigidbody.MovePosition(newPosition);
     }
 
     private void OnCollisionEnter2D(Collision2D other)
     {
-        if (other.collider.CompareTag("Obstacle"))
-            FindObjectOfType<GameMaster>().EndGame();
+        if (!other.collider.CompareTag("Finish"))
+            _gameMaster.EndGame();
     }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.CompareTag("Goal"))
-            FindObjectOfType<GameMaster>().IncrementScore();
+        if (other.CompareTag("Finish"))
+            _gameMaster.IncrementScore();
     }
 
 }

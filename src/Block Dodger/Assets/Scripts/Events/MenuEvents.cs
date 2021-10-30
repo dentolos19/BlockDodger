791d428916616bdcb4967b6bf2d18565b58d5be7
Dennise Catolos <dentolos19@gmail.com>
2021-10-30 18:14:56 +0800

    Updated demo

--- src/Block Dodger/Assets/Scripts/Events/MenuEvents.cs
+++ src/Block Dodger/Assets/Scripts/Events/MenuEvents.cs
@@ -4,9 +4,6 @@ using UnityEngine.SceneManagement;
 public class MenuEvents : MonoBehaviour
 {
 
-    public GameObject mainMenu;
-    public GameObject optionsMenu;
-
     public void OnPlay()
     {
         SceneManager.LoadScene(1);
@@ -15,18 +12,6 @@ public class MenuEvents : MonoBehaviour
     public void OnExit()
     {
         Application.Quit();
-    }
-
-    public void OnShowMain()
-    {
-        mainMenu.SetActive(true);
-        optionsMenu.SetActive(false);
-    }
-
-    public void OnShowOptions()
-    {
-        mainMenu.SetActive(false);
-        optionsMenu.SetActive(true);
     }
 
 }
